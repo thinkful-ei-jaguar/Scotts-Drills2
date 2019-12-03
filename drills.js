472e7290dc4f24a7e8ce837ae921aac57499a205
Scott Lingner <slingner@gmail.com>
2019-12-02 17:26:04 -0800

    working on crack the code

--- drills.js
+++ drills.js
@@ -7,16 +7,34 @@
 // jediName("Scott", "Lingner");
 
 
-function beyond (num) {
-    if (num === Infinity || num === Number.NEGATIVE_INFINITY) {
-        console.log('And beyond');
-    } else if (typeof num === "number" && num > 0) {
-        console.log('To infinity');
-    } else if (typeof num === "number" && num < 0) {
-        console.log('To negative infinity');
-    } else if (num === 0) {
-        console.log('Staying home');
+// function beyond (num) {
+//     if (num === Infinity || num === Number.NEGATIVE_INFINITY) {
+//         console.log('And beyond');
+//     } else if (typeof num === "number" && num > 0) {
+//         console.log('To infinity');
+//     } else if (typeof num === "number" && num < 0) {
+//         console.log('To negative infinity');
+//     } else if (num === 0) {
+//         console.log('Staying home');
+//     }
+// }
+
+// beyond(0)
+
+
+
+function decode (word) {
+    // let a = word.charAt(2);
+    // let b = word.charAt(3);
+    // let c = word.charAt(4);
+    // let d = word.charAt(5);
+    let firstLetter = word.charAt(0);
+    let decodeLetter = word.charAt(firstLetter);
+    if (typeof firstLetter > "d") {
+        console.log(" ");
+    } else {
+        console.log(decodeLetter);
     }
 }
 
-beyond(0)
+decode("Mouse");
