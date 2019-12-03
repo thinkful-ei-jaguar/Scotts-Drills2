e30af528574b894979c78612901d14409586d3ef
Scott Lingner <slingner@gmail.com>
2019-12-02 16:20:55 -0800

    Add to infinity drill

--- drills.js
+++ drills.js
@@ -1,8 +1,22 @@
-function jediName (firstName, lastName) {
-    let first = firstName.slice(0,2);
-    let last = lastName.slice(0,3);
-    console.log(`${last}${first}`)
+// function jediName (firstName, lastName) {
+//     let first = firstName.slice(0,2);
+//     let last = lastName.slice(0,3);
+//     console.log(`${last}${first}`)
+// }
+
+// jediName("Scott", "Lingner");
+
+
+function beyond (num) {
+    if (num === Infinity || num === Number.NEGATIVE_INFINITY) {
+        console.log('And beyond');
+    } else if (typeof num === "number" && num > 0) {
+        console.log('To infinity');
+    } else if (typeof num === "number" && num < 0) {
+        console.log('To negative infinity');
+    } else if (num === 0) {
+        console.log('Staying home');
+    }
 }
 
-jediName("Scott", "Lingner");
-
+beyond(0)
